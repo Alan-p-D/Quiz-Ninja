721e8858d7d417b75ec6e62a344fd499d5bd8c12
Alan Deverill <alan36pd@gmx.co.uk>
2018-03-24 15:37:17 +0000

    removed unsued code

--- main.js
+++ main.js
@@ -1,13 +1,6 @@
 alert('Welcom to the Quiz Ninja!');
 
 
-//const message = 'Hello World!';
-//alert(message);
-
-//const question = "What is Superman's real name?";
-//const answer = prompt(question);
-//alert('You answered ${answer}');
-
 const quiz = [  // quiz array including questions and answers
     ["What is superman's real name?", "Clark Kent"],
     ["What is wonder woman's real name?", "Diana Prince"],
@@ -46,17 +39,4 @@ for (const [question,answer] of quiz){
         alert(`Game over, you scored ${score} point${score != 1 ? 's' : ''}`);
     }
     start(quiz);
-    /* code from previous chapter
-
-    if(response === answer){
-        alert('Correct');
-        score++;
-    } else {
-        alert(`wrong! the correct answer was ${answer}`);
-    }
-}
-
-// At the end of the game, report the player's score.
-    alert(`Game over, you scored ${score} point${score !== 1 ? 's' : ''}`);
-
-*/
+   
