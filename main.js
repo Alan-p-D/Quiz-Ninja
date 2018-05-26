7ba60fe4eaa6286a5e416377015c3cb6e8dc65f5
Alan Deverill <alan36pd@gmx.co.uk>
2018-05-26 16:58:25 +0100

    updated code
    Streamlined code

--- main.js
+++ main.js
@@ -1,13 +1,51 @@
 alert('Welcom to the Quiz Ninja!');
 
+const quiz = [
+    {name: "Superman", realName: "Clark Kent"},
+    {name: "Wonder Woman", realName: "Diana Prince"},
+    {name: "Batman", realName: "Bruce Wayne"},
+];
 
-const quiz = [  // quiz array including questions and answers
+const game = {
+    start(quiz){
+        this.question = [...quiz];
+        this.score = 0;
+        // main game loop
+        for(const question of this.question){
+            this.question = question;
+            this.ask();
+        }
+        // end of main loop
+        this.gameOver();
+    },
+    ask() {
+        const question = `What is ${this.question.name}'s real name?`;
+        const response = prompt(question);
+        this.check(response);
+    },
+    check(response){
+        const answer = this.question.realName;
+        if(response === answer){
+            alert('Correct!');
+            this.score++;
+        } else {
+            alert(`Wrong! The correct answer was ${answer}`);
+        }
+    },
+    gameOver(){
+        alert(`Game over, You scored ${this.score} points${this.score !== ? 's' : ''}`);
+    }
+}
+
+game.start(quiz);
+
+/*const quiz = [  // quiz array including questions and answers
     ["What is superman's real name?", "Clark Kent"],
     ["What is wonder woman's real name?", "Diana Prince"],
     ["what is batman's real name?", "Bruce Wayne"]
-];
+];*/
 
-
+/*
 function start (quiz) {
     let score = 0; // initialise score
 }
@@ -22,7 +60,7 @@ for (const [question,answer] of quiz){
     gameOver();
 
     // function declarations
-    function ask(question) {
+   function ask(question) {
         return prompt(question);
     }
 
@@ -38,5 +76,5 @@ for (const [question,answer] of quiz){
     function gameOver() {
         alert(`Game over, you scored ${score} point${score != 1 ? 's' : ''}`);
     }
-    start(quiz);
+    start(quiz); */
    
